b18afbb108e09d4e13be0d14e9b9d1fa14ce02ff
MSP <mario@silvapereira.com>
2024-10-07 23:35:12 +0100

    Hopefully: cleanup commits merge to osxphotos issue 1703

--- osxphotos/exif_datetime_updater.py
+++ osxphotos/exif_datetime_updater.py
@@ -178,9 +178,9 @@ class ExifDateTimeUpdater:
             )
             return None
 
-        self.verbose(f"[bold]{dtinfo=} {type(dtinfo)=} {dtinfo.offset_seconds=}[/bold]")
+        print(f"{dtinfo=} {type(dtinfo)=} {dtinfo.offset_seconds=}")
 
-        if dtinfo.offset_seconds or dtinfo.offset_seconds == 0:
+        if dtinfo.offset_seconds is not None:
             # update timezone then update date/time
             timezone = Timezone(dtinfo.offset_seconds)
             tzupdater = PhotoTimeZoneUpdater(
